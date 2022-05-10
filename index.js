e40e8c366d6ce126ec8a7fc9f5b02e94dc169fa1
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-05-10 12:47:58 +0200

    create random meteors

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ container.addEventListener("mousemove", (e) => {
   mouseX = e.pageX;
   rocket.style.left = mouseX + "px";
 });
+
+function makeMeteor() {
+  meteorCount = 50;
+  i = 0;
+
+  while (i < meteorCount) {
+    meteor = document.createElement("i");
+    meteor.classList.add("meteor");
+    windowX = Math.floor(Math.random() * window.innerWidth);
+
+    duration = Math.random() * 1;
+
+    meteorWidth = Math.random() * 3;
+    meteorHeight = Math.random() * 150;
+
+    meteor.style.left = windowX + "px";
+    meteor.style.width = meteorWidth + "px";
+    meteor.style.height = meteorHeight + "px";
+    meteor.style.animationDuration = duration + "s";
+
+    container.appendChild(meteor);
+    i++;
+  }
+}
+
+makeMeteor();
